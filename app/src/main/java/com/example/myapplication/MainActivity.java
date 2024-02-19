a13cc143b57a72dace7b54cfaf1a8c23d4d8ca66
Nastya <1764347@bk.ru>
2024-02-19 11:47:53 +0300

    commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.myapplication;
+
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.example.myapplication.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+
+    public static final String EXTRA_MESSAGE = "com.example.myapplication.MESSAGE";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+
+        setContentView(binding.getRoot());
+
+        binding.editText.findViewById(R.id.editText);
+        binding.mainButton.findViewById(R.id.main_button);
+        binding.mainButton.setOnClickListener(v->sendMessage());
+
+    }
+
+    public void sendMessage() {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        String message = binding.editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+}
